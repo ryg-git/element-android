17610dc7ce5b42145f46d31bada4de9d8bd9ae6c
ariskotsomitopoulos <aris.kotsomitopoulos@gmail.com>
2021-09-27 18:21:38 +0300

    Update copyright

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/model/presence/UserPresence.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/api/session/room/model/presence/UserPresence.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 New Vector Ltd
+ * Copyright 2021 The Matrix.org Foundation C.I.C.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.matrix.android.sdk.api.session.room.model.presence
 
 import org.matrix.android.sdk.internal.session.presence.messages.PresenceEnum
